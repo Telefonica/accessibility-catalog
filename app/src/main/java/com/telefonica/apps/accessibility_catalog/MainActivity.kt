a04f354c4bf058ca99ccb604a22bd13f040ec85c
Fernando Garcia <fernando.garcialidon@telefonica.com>
2024-02-26 16:46:20 +0100

    Update. Apply Telefonica brand

--- app/src/main/java/com/telefonica/apps/accessibility_catalog/MainActivity.kt
+++ app/src/main/java/com/telefonica/apps/accessibility_catalog/MainActivity.kt
@@ -11,16 +11,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.telefonica.apps.accessibility_catalog.ui.theme.AccessibilityCatalogTheme
+import com.telefonica.mistica.button2.Button
+import com.telefonica.mistica.compose.theme.MisticaTheme
+import com.telefonica.mistica.compose.theme.brand.TelefonicaBrand
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AccessibilityCatalogTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                Greeting("Android")
             }
         }
     }
