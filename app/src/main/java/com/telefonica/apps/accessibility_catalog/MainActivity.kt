c7b174e4bf18d6240b533583e4f3bdff1bb5942a
Fernando Garcia <fernando.garcialidon@telefonica.com>
2024-04-25 12:55:47 +0200

    Remove. Default composables

--- app/src/main/java/com/telefonica/apps/accessibility_catalog/MainActivity.kt
+++ app/src/main/java/com/telefonica/apps/accessibility_catalog/MainActivity.kt
@@ -20,21 +20,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AccessibilityCatalogTheme {
-                Greeting("Android")
+                
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AccessibilityCatalogTheme {
-        Greeting("Android")
+
     }
 }
